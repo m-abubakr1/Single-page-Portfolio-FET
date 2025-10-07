7fdb56f3454a30717bada551fceb0a5e6e4ad8cb
M. Abubakr <mabubakr429@gmail.com>
2025-10-07 21:59:26 +0500

    Styling the Cards in Projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -61,11 +61,14 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8"
             >
-              <div>
-                <span>{project.company}</span>
-                <span>{project.year}</span>
+              <div className="flex">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
               </div>
               <h3>{project.title}</h3>
               <hr />
