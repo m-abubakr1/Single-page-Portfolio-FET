c5296c128f2e5bf92b9a262ebe84d94fc4853147
M. Abubakr <mabubakr429@gmail.com>
2025-10-07 21:53:05 +0500

    Styling the Cards in Projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -61,7 +61,7 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20"
             >
               <div>
                 <span>{project.company}</span>
