6b44743e73545c98722d30f1a5edd5ac6708ee34
M. Abubakr <mabubakr429@gmail.com>
2025-10-08 20:45:13 +0500

    Update Testimonial Section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -5,6 +5,7 @@ import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import { SectionHeader } from "@/components/SectionHeader";
 import Image from "next/image";
+import grainImage from "@/assets/images/grain.jpg";
 
 const testimonials = [
   {
@@ -41,22 +42,33 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div>
-      <SectionHeader
-        eyebrow="Happy Clients"
-        title="What CLients say about Me"
-        description="Don't just take my words for it. See what my clients have to say about
+    <div className="py-16">
+      <div className="container">
+        <SectionHeader
+          eyebrow="Happy Clients"
+          title="What CLients say about Me"
+          description="Don't just take my words for it. See what my clients have to say about
         my work."
-      />
-      <div>
-        {testimonials.map((testimonial) => (
-          <div key={testimonial.name}>
-            <Image src={testimonial.avatar} alt={testimonial.name} />
-            <h3>{testimonial.name}</h3>
-            <h4>{testimonial.position}</h4>
-            <p>"{testimonial.text}"</p>
-          </div>
-        ))}
+        />
+        <div>
+          {testimonials.map((testimonial) => (
+            <div
+              key={testimonial.name}
+              className="bg-gray-800 rounded-3xl p-6 relative overflow-hidden z-0"
+            >
+              <div
+                className="absolute inset-0 opacity-5 -z-10"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <Image src={testimonial.avatar} alt={testimonial.name} />
+              <h3>{testimonial.name}</h3>
+              <h4>{testimonial.position}</h4>
+              <p>"{testimonial.text}"</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
