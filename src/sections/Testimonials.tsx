23367c6508c2c61489df460e49e7f515c37c54f7
M. Abubakr <mabubakr429@gmail.com>
2025-10-09 01:03:55 +0500

    Update Testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-4">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Clients"
@@ -51,15 +51,31 @@ export const TestimonialsSection = () => {
           description="Don't just take my words for it. See what my clients have to say about
         my work."
         />
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <h3>{testimonial.name}</h3>
-              <h4>{testimonial.position}</h4>
-              <p>"{testimonial.text}"</p>
-            </Card>
-          ))}
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs md:p-8">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">{testimonial.name}</h3>
+                    <h4 className="text-sm text-white/40">
+                      {testimonial.position}
+                    </h4>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm md:text-base md:mt-6">
+                  "{testimonial.text}"
+                </p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
