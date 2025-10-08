5709c35398dc34e3293b25c9165c1369f3d5bac0
M. Abubakr <mabubakr429@gmail.com>
2025-10-08 16:31:00 +0500

    Make responsice section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -47,7 +47,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
@@ -57,7 +57,7 @@ export const ProjectsSection = () => {
         <h2 className="text-center font-serif text-3xl md:text-5xl mt-6">
           Featured Projects
         </h2>
-        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
           See how i transformed concepts into engaging digital experiences.
         </p>
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
