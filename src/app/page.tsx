aa5da071bdae12d330a7477328f549378b29f697
M. Abubakr <mabubakr429@gmail.com>
2025-10-09 22:58:50 +0500

    Add About Section in pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { AboutSection } from "@/sections/About";
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
+import { AboutSection } from "@/sections/About";
 
 export default function Home() {
   return (
