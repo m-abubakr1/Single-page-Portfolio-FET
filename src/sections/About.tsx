e618f4dedbe2b4ad3ee07643e4c07ae705a593a0
M. Abubakr <mabubakr429@gmail.com>
2025-10-09 23:22:05 +0500

    Add Array data and COmponents into About Section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,74 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    icon: <JavascriptIcon />,
+  },
+  {
+    title: "HTML5",
+    icon: <HTMLIcon />,
+  },
+  {
+    title: "CSS3",
+    icon: <CssIcon />,
+  },
+  {
+    title: "React",
+    icon: <ReactIcon />,
+  },
+  {
+    title: "Chrome",
+    icon: <ChromeIcon />,
+  },
+  {
+    title: "Github",
+    icon: <GithubIcon />,
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div>
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description="Learn more about who I am, what I do, and what inspires me."
+      />
+      <div>
+        {/* Reading card */}
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my Perspectives.</p>
+          </div>
+          <Image src={bookImage} alt="Book Cover" />
+        </Card>
+
+        {/* ToolBox card */}
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My ToolBox</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional
+              digital experiences.
+            </p>
+          </div>
+          <div></div>
+        </Card>
+      </div>
+    </div>
+  );
 };
